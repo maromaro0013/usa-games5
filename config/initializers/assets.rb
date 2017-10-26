c0905d8a79ffdcb4d3f4057ae5f066b3f9734dc1
maromaro0013 <ota0120@gmail.com>
2017-10-26 19:44:37 +0900

    toppageも移植することにした

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,5 +13,5 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 
-Rails.application.config.assets.precompile += %w( games/*.js )
-Rails.application.config.assets.precompile += %w( games/*.css )
+Rails.application.config.assets.precompile += %w( webpages/*.js games/*.js )
+Rails.application.config.assets.precompile += %w( webpages/*.css games/*.css )
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  get '/' => 'toppage#index', as: :toppage_index
+
   get '/games/rescue/:level' => 'games#rescue', as: :games_rescue
 
   post '/games/rescue/hakoirisolver' => 'games#hakoirisolver', as: :games_rescue_hakoirisolver
